55a3ea770a99db2a3dbdba5bfa8de235e1094f3a
VietAnh270902 <95521603+VietAnh270902@users.noreply.github.com>
2022-03-09 12:26:27 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bài_tập_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QLNV.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bài_tập_net
+{
+    class QLNV
+    {
+    }
+}
